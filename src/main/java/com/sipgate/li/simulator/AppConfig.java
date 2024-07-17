98e869f5b50c9086c4132aea8a3e72de648b72f5
Jannik Volkland <volkland@sipgate.de>
2024-07-17 15:36:16 +0200

    ci: add first e2e tests

--- src/main/java/com/sipgate/li/simulator/AppConfig.java
+++ src/main/java/com/sipgate/li/simulator/AppConfig.java
@@ -3,6 +3,8 @@ package com.sipgate.li.simulator;
 import com.sipgate.li.lib.x1.X1Client;
 import com.sipgate.li.lib.x1.X1RequestFactory;
 import jakarta.xml.bind.JAXBException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,8 @@ import java.net.http.HttpClient;
 @Configuration
 public class AppConfig {
 
+    private Logger LOGGER = LoggerFactory.getLogger(AppConfig.class);
+
     @Bean
     public X1RequestFactory x1RequestFactory(
             @Value("${sipgate.li.simulator.neUri}") final URI neUri,
@@ -25,6 +29,8 @@ public class AppConfig {
 
     @Bean
     public X1Client x1Client(@Value("${sipgate.li.simulator.neUri}") final URI neUri) throws JAXBException {
+
+        LOGGER.info("Attempting to create connections to {}.", neUri.toString());
         return new X1Client(neUri, HttpClient.newHttpClient());
     }
 }
---
+++ src/test/java/com/sipgate/li/simulator/e2e/PingControllerE2eTest.java
@@ -0,0 +1,24 @@
+package com.sipgate.li.simulator.e2e;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PingControllerE2eTest extends E2eTestCase {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/ping", "/keepalive"})
+    void itReturns200ToRequests(final String path) throws IOException, InterruptedException {
+        // WHEN
+        final var response = sendUnauthenticated(path);
+
+        // THEN
+        assertThat(response.statusCode()).isEqualTo(200);
+
+        final var responseBody = objectMapper.readTree(response.body());
+        assertThat(responseBody.get("ok").asText()).isEqualTo("ACKNOWLEDGED_AND_COMPLETED");
+    }
+}

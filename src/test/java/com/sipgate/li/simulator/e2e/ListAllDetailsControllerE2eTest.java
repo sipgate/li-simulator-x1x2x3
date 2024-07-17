d7c8d8b8e276dd0c59a806e722be485b152ff807
Jannik Volkland <volkland@sipgate.de>
2024-07-17 17:44:15 +0200

    feat: add list all details command

---
+++ src/test/java/com/sipgate/li/simulator/e2e/ListAllDetailsControllerE2eTest.java
@@ -0,0 +1,29 @@
+package com.sipgate.li.simulator.e2e;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ListAllDetailsControllerE2eTest extends E2eTestCase {
+
+
+    @Test
+    void itReturns200ToRequests() throws IOException, InterruptedException {
+        // WHEN
+        final var response = sendUnauthenticated("/listAllDetails");
+
+        // THEN
+        assertThat(response.statusCode()).isEqualTo(200);
+
+        final var responseBody = objectMapper.readTree(response.body());
+        assertThat(responseBody.get("tasks").isArray()).isTrue();
+        assertThat(responseBody.get("tasks").get(0).asText()).isEqualTo("29f28e1c-f230-486a-a860-f5a784ab9172");
+        assertThat(responseBody.get("tasks").get(1).asText()).isEqualTo("4f79fd4a-b237-48c3-b5d3-334ed9191f33");
+
+        assertThat(responseBody.get("destinations").isArray()).isTrue();
+        assertThat(responseBody.get("destinations").get(0).asText()).isEqualTo("b0ce308c-aa17-42bd-a27b-287bcb5b3468");
+        assertThat(responseBody.get("destinations").get(1).asText()).isEqualTo("6d44e794-707a-4338-8c43-15f4213f508a");
+    }
+}

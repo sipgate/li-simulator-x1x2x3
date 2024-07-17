8d4799b44735f0b1541d488942e283303ba047ed
Jannik Volkland <volkland@sipgate.de>
2024-07-17 12:17:59 +0200

    docs: create richer swagger documentation

--- src/main/java/com/sipgate/li/simulator/PingController.java
+++ src/main/java/com/sipgate/li/simulator/PingController.java
@@ -2,17 +2,17 @@ package com.sipgate.li.simulator;
 
 import com.sipgate.li.lib.x1.X1Client;
 import com.sipgate.li.lib.x1.X1RequestFactory;
-import jakarta.xml.bind.JAXBException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.etsi.uri._03221.x1._2017._10.OK;
 import org.etsi.uri._03221.x1._2017._10.PingRequest;
 import org.etsi.uri._03221.x1._2017._10.PingResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
 
 @Controller
 @ResponseBody
@@ -26,17 +26,35 @@ public class PingController {
         this.x1Client = x1Client;
     }
 
-    @PostMapping("/ping")
-    public ResponseEntity<Map<String, String>> ping() throws JAXBException, IOException, InterruptedException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    @Operation(summary = "PingRequest", description = "Send a ping request to the NE.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ping to the NE was returned.", content = @Content(examples = @ExampleObject("""
+                    {"ok": "AcknowledgedAndCompleted"}
+                    """))),
+            @ApiResponse(responseCode = "500", description = "The PingRequest was not returned properly.", content = @Content(examples = @ExampleObject("""
+                    {"error": "PingRequest did not respond with PingResponse, received ActivateTaskResponse"}
+                    """)))
+    })
+    @GetMapping("/ping")
+    public ResponseEntity<Response> ping() throws Exception {
 
         final var req = x1RequestFactory.create(PingRequest.class);
         final var resp = x1Client.request(req);
 
         if (resp instanceof PingResponse p) {
-            return ResponseEntity.ok(Map.of("ok", p.getOK().value()));
+            return ResponseEntity.ok(Response.ok(p.getOK()));
         }
 
-        return ResponseEntity.internalServerError().body(Map.of("error", "PingRequest did not respond with PingResponse, received " + resp.getClass().getSimpleName()));
+        return ResponseEntity.internalServerError().body(Response.error("PingRequest did not respond with PingResponse, received " + resp.getClass().getSimpleName()));
     }
 
+    record Response(String ok, String error) {
+        static Response ok(OK ok) {
+            return new Response(ok.value(), null);
+        }
+
+        static Response error(String m) {
+            return new Response(null, m);
+        }
+    }
 }

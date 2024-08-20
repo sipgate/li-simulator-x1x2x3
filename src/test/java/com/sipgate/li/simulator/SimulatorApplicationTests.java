8084716e694ec841cbf5d802e671c86cc7b7cb41
Jannik Volkland <volkland@sipgate.de>
2024-08-20 15:41:00 +0200

    test(ci): only run SpringBootTest in E2E
    Co-authored-by: Sven Uhlig <uhlig@sipgate.de>
    Co-authored-by: Lennart Rosam <rosam@sipgate.de>

--- src/test/java/com/sipgate/li/simulator/SimulatorApplicationTests.java
+++ src/test/java/com/sipgate/li/simulator/SimulatorApplicationTests.java
@@ -1,8 +1,10 @@
 package com.sipgate.li.simulator;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Tag("E2E")
 @SpringBootTest
 class SimulatorApplicationTests {
 

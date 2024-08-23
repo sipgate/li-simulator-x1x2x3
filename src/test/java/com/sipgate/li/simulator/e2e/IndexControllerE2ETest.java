536596059a31814976e4e1d193514468f6063e51
Sven Uhlig <uhlig@sipgate.de>
2024-08-23 15:46:56 +0200

    test(index): also check created xId

--- src/test/java/com/sipgate/li/simulator/e2e/IndexControllerE2ETest.java
+++ src/test/java/com/sipgate/li/simulator/e2e/IndexControllerE2ETest.java
@@ -3,7 +3,9 @@ package com.sipgate.li.simulator.e2e;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.sipgate.li.simulator.controller.IndexController.IndexResponse;
+import com.sipgate.li.simulator.controller.response.TaskActivatedResponse;
 import java.io.IOException;
+import java.util.Map;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,12 +15,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 class IndexControllerE2ETest {
 
   @Test
-  void itReturns200ToRequests(final SimulatorClient client)
+  void itReturnsCreatedXId(final SimulatorClient client)
     throws IOException, InterruptedException {
+    // GIVEN
+    final var taskActivatedResponse = client.post(
+      "/task",
+      Map.of("e164number", "4915799912345", "destinationId", "pre-shared-did"),
+      TaskActivatedResponse.class
+    );
+
     // WHEN
-    final var response = client.get("/index", IndexResponse.class);
+    final var indexResponse = client.get("/index", IndexResponse.class);
 
     // THEN
-    assertThat(response.tasks()).isNotNull();
+    assertThat(indexResponse.tasks()).isNotNull();
+    assertThat(indexResponse.tasks()).contains(taskActivatedResponse.xId());
   }
 }

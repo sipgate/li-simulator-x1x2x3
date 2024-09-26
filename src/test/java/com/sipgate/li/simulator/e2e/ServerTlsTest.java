f7cc2b2639b2232e0c381b048ce7dcdcf9452e8e
Nick Garczorz <garczorz@sipgate.de>
2024-09-26 15:58:58 +0200

    feat(x2x3ServerTest): Update test certs

--- src/test/java/com/sipgate/li/simulator/e2e/ServerTlsTest.java
+++ src/test/java/com/sipgate/li/simulator/e2e/ServerTlsTest.java
@@ -1,29 +1,89 @@
 package com.sipgate.li.simulator.e2e;
 
+import static com.sipgate.li.lib.x2x3.PduObject.MANDATORY_HEADER_LENGTH;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
+import com.sipgate.li.lib.x2x3.*;
+import com.sipgate.li.simulator.x2x3.X2X3InboundHandlerAdapter;
+import com.sipgate.li.simulator.x2x3.X2X3Server;
+import com.sipgate.util.SSLContextBuilder;
 import java.io.IOException;
-import java.util.stream.Stream;
-import org.etsi.uri._03221.x1._2017._10.KeepaliveResponse;
-import org.etsi.uri._03221.x1._2017._10.OK;
-import org.etsi.uri._03221.x1._2017._10.PingResponse;
-import org.etsi.uri._03221.x1._2017._10.X1ResponseMessage;
+import java.nio.file.Path;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.util.UUID;
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLContext;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.ApplicationEventPublisher;
 
-//TODO doesnt work.
-// @SpringBootTest("spring.profiles.active=test")
 @Tag("E2E")
 class ServerTlsTest {
 
+  private final ApplicationEventPublisher applicationEventPublisher = mock(ApplicationEventPublisher.class);
+  private final X2X3InboundHandlerAdapter x2x3InboundHandlerAdapter = new X2X3InboundHandlerAdapter(
+    applicationEventPublisher
+  );
+  private final X2X3Decoder x2X3Decoder = new X2X3Decoder(2000, 2000);
+  private final SSLContext serverSslContext = serverSslContext();
+  private final SSLContext clientSslContext = clientSslContext();
+
+  ServerTlsTest()
+    throws NoSuchAlgorithmException, UnrecoverableKeyException, CertificateException, KeyStoreException, IOException, NoSuchProviderException, KeyManagementException {}
+
   @Test
-  void it_starts_spring_context() {}
+  void it_connects_to_x2x3server() throws Exception {
+    X2X3Server server = new X2X3Server(x2x3InboundHandlerAdapter, x2X3Decoder, serverSslContext);
+
+    Thread t = new Thread() {
+      @Override
+      public void run() {
+        try {
+          server.run();
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+      }
+    };
+    t.start();
+    Thread.sleep(1000);
+
+    SocketFactory clientSocketFactory = clientSslContext.getSocketFactory();
+    final var client = new X2X3Client(clientSocketFactory, "localhost", 42069);
+    PduObject pdu = new PduObject(
+      (short) 0,
+      (short) 5,
+      PduType.X2_PDU,
+      MANDATORY_HEADER_LENGTH,
+      0,
+      PayloadFormat.SIP,
+      PayloadDirection.SENT_FROM_TARGET,
+      UUID.randomUUID(),
+      new byte[8],
+      new byte[0],
+      new byte[0]
+    );
+    client.send(pdu);
+    t.join();
+
+    System.err.println("Je voir here an tomasE");
+  }
+
+  SSLContext serverSslContext()
+    throws UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, NoSuchProviderException, KeyManagementException {
+    return SSLContextBuilder.newBuilder()
+      .withKeyStore(Path.of("src/test/resources/server-keystore-test.p12"), "changeit")
+      .withTrustStore(Path.of("src/test/resources/server-truststore-test.jks"), "changeit")
+      .build();
+  }
+
+  SSLContext clientSslContext()
+    throws UnrecoverableKeyException, CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, NoSuchProviderException, KeyManagementException {
+    return SSLContextBuilder.newBuilder()
+      .withKeyStore(Path.of("src/test/resources/client-keystore-test.p12"), "changeit")
+      .withTrustStore(Path.of("src/test/resources/client-truststore-test.jks"), "changeit")
+      .build();
+  }
 }
